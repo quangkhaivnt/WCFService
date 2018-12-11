ace2ecdbffcbb2c187c2e73aa9e3af7ac76d2dfd
khainqd <khainqd00575@fpt.edu.vn>
2018-12-11 10:31:10 +0700

    WCF Service

---
+++ WCFService/IService1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+             ResponseFormat = WebMessageFormat.Json,
+             BodyStyle = WebMessageBodyStyle.Bare,
+             UriTemplate = "GetProductList/")]
+
+        List<Customer> GetCustomersList();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "PostCustomer")]
+        bool AddCustomer(Customer cus);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "PutCustomer")]
+        bool UpdateCustomer(Customer cus);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "DeleteCustomer")]
+        bool DeleteCustomer(int idC);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WCFService/Service1.svc.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        DataEmployeeDataContext data = new DataEmployeeDataContext();
+
+        public bool AddCustomer(Customer cus)
+        {
+            try
+            {
+                data.Customers.InsertOnSubmit(cus);
+                data.SubmitChanges();
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCustomer(int idC)
+        {
+            try
+            {
+                Customer cusToDelete = (from customer in data.Customers where customer.EmpID == idC select customer).Single();
+                data.Customers.DeleteOnSubmit(cusToDelete);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public List<Customer> GetCustomersList()
+        {
+            try
+            {
+                return (from customer in data.Customers select customer).ToList();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public bool UpdateCustomer(Customer cus)
+        {
+            Customer customerToModify = (from customer in data.Customers where customer.EmpID == cus.EmpID select customer).Single();
+            customerToModify.Age = cus.Age;
+            customerToModify.Address = cus.Address;
+            customerToModify.FirstName = cus.FirstName;
+            customerToModify.LastName = cus.LastName;
+            data.SubmitChanges();
+            return true;
+        }
+    }
+}
